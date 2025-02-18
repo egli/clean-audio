5c216042b8ea678bad13d050658e5a1c91328375
Christian Egli <christian.egli@sbs.ch>
2025-02-18 09:29:49 +0100

    Use wav files instead of mp3

--- clean_audio.py
+++ clean_audio.py
@@ -13,7 +13,7 @@ full_audio_path = args.audio_file
 full_audio, sr = librosa.load(full_audio_path, sr=None)
 
 # Load the sample that is to be removed
-music_sample_path = "music_sample.mp3" 
+music_sample_path = "unwanted.wav"
 music_sample, _ = librosa.load(music_sample_path, sr=sr)
 
 # Calculate cross-correlation for similarity detection
@@ -24,7 +24,7 @@ threshold = 0.6 * np.max(correlation)
 matches = np.where(correlation > threshold)[0]
 
 # Load original audio with pydub for easier slicing
-audio_segment = AudioSegment.from_file(full_audio_path, format="mp3")
+audio_segment = AudioSegment.from_file(full_audio_path, format="wav")
 
 # Remove detected segments
 for start in reversed(matches):
